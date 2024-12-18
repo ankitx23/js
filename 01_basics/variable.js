cf70d41eadb0639d1b83ed362cfff4921c12e75b
ANKIT <ankitxydv@gmail.com>
2024-12-18 02:39:40 +0000

    container commit

---
+++ 01_basics/variable.js
@@ -0,0 +1,21 @@
+const accountId = "144553"
+let accountEmail = "ankit@abc.com"
+var accountPassword = "22334455"
+accountCity= "Jaipur"
+
+let accountState
+
+// accountId=2
+/*
+prefer not to use var 
+because of issue in block scope and fucntional scope
+*/
+
+accountEmail= "ankity@gmail.com"
+accountPassword= "1234567"
+accountCity= "bengaluru"
+
+
+console.log(accountId)
+console.table([accountId,accountCity,accountEmail,accountPassword,accountState]);
+
