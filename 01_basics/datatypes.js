3306251aa3ac1f44445ea7095c4eb35f60440312
ANKIT <ankitxydv@gmail.com>
2024-12-22 00:44:40 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+/* alert("hello") */
+
+console.log("ANKIT"); console.log("jack")
+
+//code should be readable and it should be according to the standards
+
+console.log("Ankit") 
+console.log("Jack")
+
+let name = "Ankit"
+let age = "14"
+let isLoggedIn= false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standalone value
+//symbol=> unique
+
+
+//object
+
+console.log(typeof Ankit)
+console.log(typeof 23)
+console.log(typeof "")
+console.log(typeof null)
+
+
+
