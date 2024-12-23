09acf9aca38b94f9ee64e8e0cfbedb2d0e04de81
ANKIT <ankitxydv@gmail.com>
2024-12-23 01:41:30 +0000

    Conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,63 @@
+let score = "33abc"
+
+//console.log(typeof (score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// 33 => Number
+//"33" => typeof- string, valueInNumber=> Number
+//"33abc" => typeof - string,valueInNumber=> Number, NaN(Not a Number)
+//null => object, Number, 0
+// true => boolean, number , 1.
+// false=> boolean, number , 0.
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1- true
+//0 - false
+// "ankit" , true
+
+//*********************OPERATIONS************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2)
+//console.log (2*2)
+//console.log (3**4)
+//console.log (2-5)
+//console.log (2%2)
+//console.log (2/2)
+
+// WAP to add two strings
+let str1= "Hi"
+let str2= " ankit"
+
+let str3 = str1 + str2
+
+//console.log(str3)
+
+//assignment operator
+
+let num1, num2, num3
+
+num1= num2 = num3 = 2+2
+
+//console.log(num1);
+
+let gameCounter=102
+gameCounter++;
+
+console.log(gameCounter);
+
+// link https://tc39.es/ecma262/multipage/abstract-operations.html
+
+
+
+
