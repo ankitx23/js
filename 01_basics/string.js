2cc26b745d46d5b3831864aa12fb043f5c1ba255
ANKIT <ankitxydv@gmail.com>
2024-12-28 22:18:56 +0000

    String

---
+++ 01_basics/string.js
@@ -0,0 +1,50 @@
+const name = "Ankit"
+const repoCount = 50
+// console.log (name +repoCount+ "Value") //old method 
+//backticks is new method (`)
+
+//console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName =  new String('Ankit-xD');
+
+//console.log (gameName[2]);
+//console.log (gameName.__proto__);
+
+
+//console.log (gameName.length);
+//console.log (gameName.toLowerCase());
+//console.log (gameName.toUpperCase());
+//console.log (gameName.charAt(2));
+ 
+//console.log(gameName.indexOf('t'));
+
+//const anotherString = gameName.slice(0,4) // for slicing
+//console.log(anotherString);
+
+const newStringOne= "    Ankit   "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());  // trim is used to remove extra spaces from the string
+
+const url ="https://ankit.com/ankit%20yadav"
+//console.log (url.trim());
+//console.log(url.replace('%20','-'));
+
+//console.log(url.includes('ankit')) // command to ask if your keyword exists in the url or no
+//console.log(url.includes('kop'))
+
+//convert string to array
+ 
+console.log (gameName.split('-'));
+
+
+//Always follow back ticks method.
+
+
+
+
+
+
+
+
+
+
