bd6d11011d6c6000ec6ef42af63d42ea8e7641ed
ANKIT <ankitxydv@gmail.com>
2024-12-29 05:01:53 +0000

    Numbers

---
+++ 01_basics/numberMaths.js
@@ -0,0 +1,28 @@
+const score = 100;
+console.log(score);
+
+const balance= new Number(100)
+console.log (balance);
+
+console.log(balance.toString()); // converts number to string
+console.log(balance.toString().length); // converts number to string and gives the length 
+console.log(balance.toFixed(2)); // gives values after decimal
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3)); //prints number to the given digits
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()) // turns numbers acc to US standards
+console.log(hundreds.toLocaleString('en-IN')) // turns numbers acc to IN standards
+
+//-----------------------Maths------------------------//
+
+
+
+
+
+
+
+
