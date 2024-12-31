b3fa613dd9790faf602e38d51e20aa7b76bba769
ANKIT <ankitxydv@gmail.com>
2024-12-31 19:14:58 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,24 @@
+//use [] to declare an array
+const myArr = [1,2,3,4,5,6]
+const myHeroes= ["spidey",'batman']
+
+//Another way to declare an array USING KEYWORD 'ARRAY'
+const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[1]);
+
+//Array methods
+
+//to add values in array
+
+//myArr.push(6)
+//myArr.push(7)
+//myArr.pop() // removes the last value from the array
+
+//myArr.unshift(9) // adds value to an array in the beginning
+//myArr.shift() // removes the first value of an array
+
+console.log(myArr.includes(9)); // to check if value is there or no
+console.log(myArr.indexOf(4)); // to check the index of the given value
+console.log(myArr);
+
+
